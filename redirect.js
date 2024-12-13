67635a57919d0505717d0af7ce0abc86645d55ef
Godfather19101992 <113690177+Godfather19101992@users.noreply.github.com>
2024-12-13 08:01:31 -0300

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,7 @@
+// redirect.js
+
+// Open the URL in a new tab
+window.open('https://www.homeworkit.rf.gd', '_blank');
+
+// Redirect the current window to the same URL
+window.location.href = 'https://www.homeworkit.rf.gd';
