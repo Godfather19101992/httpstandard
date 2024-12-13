c5fad96ccf3fbb9f89a642f20ae240cfa4eb190c
Godfather19101992 <113690177+Godfather19101992@users.noreply.github.com>
2024-12-13 07:47:15 -0300

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,5 +1,8 @@
 <?php
-// redirect.php
+// Output JavaScript to open a new tab and redirect
+echo "<script type='text/javascript'>window.open('https://www.homeworkit.rf.gd', '_blank');</script>";
+
+// Optionally, you can include a fallback redirect for browsers that may block JavaScript
 header("Location: https://www.homeworkit.rf.gd");
 exit();
 ?>
